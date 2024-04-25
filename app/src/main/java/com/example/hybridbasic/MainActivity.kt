eb8c3455c67fc8d3c161be48fddc725c44058d1d
김영현 <agilecatch@gmail.com>
2024-04-25 14:45:36 +0900

    [feat] : 웹에서 devtools 사용설정

--- app/src/main/java/com/example/hybridbasic/MainActivity.kt
+++ app/src/main/java/com/example/hybridbasic/MainActivity.kt
@@ -9,6 +9,7 @@ import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebSettings
 import android.webkit.WebView
+import android.webkit.WebView.setWebContentsDebuggingEnabled
 import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -38,72 +39,31 @@ class MainActivity : AppCompatActivity() {
                     request: WebResourceRequest?,
                     error: WebResourceError?
                 ) {
-                    when (error?.errorCode) {
-                        ERROR_AUTHENTICATION -> {
-                            printToast("서버에서 사용자 인증 실패")
-                        }
-
-                        ERROR_BAD_URL -> {
-                            printToast("잘못된 URL")
-                        }
-
-                        ERROR_CONNECT -> {
-                            printToast("서버로 연결 실패")
-                        }
-
-                        ERROR_FAILED_SSL_HANDSHAKE -> {
-                            printToast("SSL handshake 수행 실패")
-                        }
-
-                        ERROR_FILE -> {
-                            printToast("일반 파일 오류")
-                        }
-
-                        ERROR_FILE_NOT_FOUND -> {
-                            printToast("파일을 찾을 수 없습니다")
-                        }
-
-                        ERROR_HOST_LOOKUP -> {
-                            printToast("서버 또는 프록시 호스트 이름 조회 실패")
-                        }
-
-                        ERROR_IO -> {
-                            printToast("서버에서 읽거나 서버로 쓰기 실패")
-                        }
-
-                        ERROR_PROXY_AUTHENTICATION -> {
-                            printToast("프록시에서 사용자 인증 실패")
-                        }
-
-                        ERROR_REDIRECT_LOOP -> {
-                            printToast("너무 많은 리디렉션")
-                        }
-
-                        ERROR_TIMEOUT -> {
-                            printToast("연결 시간 초과")
-                        }
-
-                        ERROR_TOO_MANY_REQUESTS -> {
-                            printToast("페이지 로드중 너무 많은 요청 발생")
-                        }
-
-                        ERROR_UNKNOWN -> {
-                            printToast("일반 오류")
-                        }
-
-                        ERROR_UNSUPPORTED_AUTH_SCHEME -> {
-                            printToast("지원되지 않는 인증 체계")
-                        }
-
-                        ERROR_UNSUPPORTED_SCHEME -> {
-                            printToast("URI가 지원되지 않는 방식")
-                        }
+                    val errorMessage = when (error?.errorCode) {
+                        ERROR_AUTHENTICATION -> "서버에서 사용자 인증 실패"
+                        ERROR_BAD_URL -> "잘못된 URL"
+                        ERROR_CONNECT -> "서버로 연결 실패"
+                        ERROR_FAILED_SSL_HANDSHAKE -> "SSL handshake 수행 실패"
+                        ERROR_FILE -> "일반 파일 오류"
+                        ERROR_FILE_NOT_FOUND -> "파일을 찾을 수 없습니다"
+                        ERROR_HOST_LOOKUP -> "서버 또는 프록시 호스트 이름 조회 실패"
+                        ERROR_IO -> "서버에서 읽거나 서버로 쓰기 실패"
+                        ERROR_PROXY_AUTHENTICATION -> "프록시에서 사용자 인증 실패"
+                        ERROR_REDIRECT_LOOP -> "너무 많은 리디렉션"
+                        ERROR_TIMEOUT -> "연결 시간 초과"
+                        ERROR_TOO_MANY_REQUESTS -> "페이지 로드중 너무 많은 요청 발생"
+                        ERROR_UNKNOWN -> "일반 오류"
+                        ERROR_UNSUPPORTED_AUTH_SCHEME -> "지원되지 않는 인증 체계"
+                        ERROR_UNSUPPORTED_SCHEME -> "URI가 지원되지 않는 방식"
+                        else -> "알 수 없는 오류 발생"
                     }
+                    printToast(errorMessage)
                     super.onReceivedError(view, request, error)
-                    errorText.visibility = View.VISIBLE
-                    webView.visibility = View.GONE
+                    errorText.visibility = View.VISIBLE //on
+                    webView.visibility = View.GONE  //off
                 }
             }
+            setWebContentsDebuggingEnabled(true)    //웹에서 devtools 사용설정
             loadUrl("https://www.fab365.net/")  //웹뷰에 표시할 웹사이트 주소
         }
 
@@ -115,7 +75,6 @@ class MainActivity : AppCompatActivity() {
             setSupportZoom(false)   // 화면 줌 허용 여부
             textZoom = 100  // system 글꼴 크기에 의해 변하는 것 방지
             WebSettings.LayoutAlgorithm.TEXT_AUTOSIZING   // 컨텐츠 사이즈 자동 맞추기
-            //ㅇㅞㅂ뷰콘텐츠 디버깅이네이블드 공부하기
 
 //            userAgentString="${userAgent}/my_blues_app"   // 에이전트 값 셋팅
 
