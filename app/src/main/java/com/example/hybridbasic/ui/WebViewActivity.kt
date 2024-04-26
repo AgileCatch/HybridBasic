db5d3cd6596ed125b7a1b166a594f8edfc293937
김영현 <agilecatch@gmail.com>
2024-04-26 11:46:54 +0900

    removed cached

--- app/src/main/java/com/example/hybridbasic/ui/WebViewActivity.kt
+++ app/src/main/java/com/example/hybridbasic/ui/WebViewActivity.kt
@@ -1,6 +1,7 @@
 package com.example.hybridbasic.ui
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.webkit.WebResourceError
@@ -21,11 +22,12 @@ class WebViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        initView()
+        initWebView()
+        initWebSetting()
     }
 
-    @SuppressLint("SetJavaScriptEnabled")
-    private fun initView() = with(binding) {
+
+    private fun initWebView() = with(binding) {
         webView.apply {
             webViewClient = object : WebViewClient() {  // 클릭 시 새창 안뜨게 페이지 탬색처리
                 override fun onReceivedError(   // 오류 발생 시
@@ -61,31 +63,42 @@ class WebViewActivity : AppCompatActivity() {
             clearCache(true)
             loadUrl("https://www.fab365.net/")  //웹뷰에 표시할 웹사이트 주소
         }
+    }
 
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initWebSetting() = with(binding) {
         val webSettings: WebSettings = webView.settings
+
         webSettings.apply {
             javaScriptEnabled = true    // 웹페이지 자바스크립트 허용 여부
-            useWideViewPort = true // 화면 사이즈 맞추기 허용 여부
-            domStorageEnabled = true  //DOM 로컬 스토리지 사용여부
-            loadWithOverviewMode = true //컨텐츠가 웹뷰보다 클때 스크린 크기에 맞추기
-            allowContentAccess = true   //웹뷰를 통해 Content URL에 접근 할지 여부
-            allowFileAccess = true   //파일 액세스 허용 여부
-            blockNetworkImage = false   //네트워크를 통해 이미지리소스 받을지 여부
-            builtInZoomControls = true  // 줌 아이콘
-            textZoom = 100  // system 글꼴 크기에 의해 변하는 것 방지
-            databaseEnabled = false   //database storage API 사용 여부
-            setSupportZoom(false)   // 화면 줌 허용 여부
             setSupportMultipleWindows(true) //멀티윈도우를 지원할지 여부
-            WebSettings.LayoutAlgorithm.TEXT_AUTOSIZING   // 컨텐츠 사이즈 자동 맞추기
+            javaScriptCanOpenWindowsAutomatically = true
+            loadWithOverviewMode = true //컨텐츠가 웹뷰보다 클때 스크린 크기에 맞추기
+            useWideViewPort = true  // 화면 사이즈 맞추기 허용 여부
+            setSupportZoom(false)    // 화면 줌 허용 여부
+            domStorageEnabled= true     //DOM 로컬 스토리지 사용여부
 
-//            userAgentString="${userAgent}/my_blues_app"   // 에이전트 값 셋팅
+            allowUniversalAccessFromFileURLs = true
+            databaseEnabled = true  //database storage API 사용 여부
+            allowFileAccess = true  //파일 액세스 허용 여부
+            textZoom = 100  // system 글꼴 크기에 의해 변하는 것 방지
+            builtInZoomControls = true  // 줌 아이콘
+            displayZoomControls = false
+
+            WebSettings.LayoutAlgorithm.TEXT_AUTOSIZING   // 컨텐츠 사이즈 자동 맞추기
+            mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            cacheMode = WebSettings.LOAD_DEFAULT
+
+            //        userAgentString="${userAgent}/my_blues_app"   // 에이전트 값 셋팅
+            //        userAgentString = userAgentString + " " + AppConst.AGENT_STR
 
         }
+
     }
+
 
     //나중에 유틸로 분리하기
     fun printToast(msg: String) {
         Toast.makeText(applicationContext, msg, Toast.LENGTH_LONG).show()
-
     }
 }
